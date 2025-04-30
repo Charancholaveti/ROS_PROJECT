cedae08e1f341f01cd43bc66c37bac339641696e
charancholaveti <cholaveticharan47@gmail.com>
2025-04-30 10:35:42 +0530

    converted 2_joint arm to 6DOF

--- install/my_robot_description/share/my_robot_description/launch/display.launch.py
+++ install/my_robot_description/share/my_robot_description/launch/display.launch.py
@@ -1,38 +1,36 @@
+import launch
 from launch import LaunchDescription
-from launch_ros.parameter_descriptions import ParameterValue
+from launch.actions import DeclareLaunchArgument, LogInfo
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
-from launch.substitutions import Command
-import os
-from ament_index_python.packages import get_package_share_path
 
 def generate_launch_description():
-
-    urdf_path = os.path.join(get_package_share_path('my_robot_description'),
-                             'urdf', 'my_robot.urdf.xacro')
-    rviz_config_path = os.path.join(get_package_share_path('my_robot_description'),
-                                    'rviz', 'urdf_config.rviz')
-    
-    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
-
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        parameters=[{'robot_description': robot_description}]
-    )
-
-    joint_state_publisher_gui_node = Node(
-        package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui"
-    )
-
-    rviz2_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        arguments=['-d', rviz_config_path]
-    )
-
     return LaunchDescription([
-        robot_state_publisher_node,
-        joint_state_publisher_gui_node,
-        rviz2_node
+        DeclareLaunchArgument('urdf_path', default_value='/home/charan/ws1/src/my_robot_description/urdf/my_robot.urdf.xacro', description='Path to the URDF file'),
+        DeclareLaunchArgument('rviz_config_path', default_value='/home/charan/ws1/src/my_robot_description/rviz/urdf_config.rviz', description='Path to the RViz config file'),
+        
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': Command(['xacro ', LaunchConfiguration('urdf_path')])
+            }]
+        ),
+        
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+        
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', LaunchConfiguration('rviz_config_path')]
+        ),
     ])
